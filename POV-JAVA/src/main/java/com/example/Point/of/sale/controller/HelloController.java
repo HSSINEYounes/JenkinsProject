1d1721c18ddcc45565697c4252ab49641ba550c2
HSSINE Younes <hssine.younes@emsi-edu.ma>
2024-01-11 20:39:37 +0100

    Another Commit

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from another TEST 101!!:)";
+        return "Hello from another TEST 202!!:)";
     }
 
     @GetMapping("/user")
